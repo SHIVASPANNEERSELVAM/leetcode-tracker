96927e263a397f2feaae63e02f76a9cb9ea73481
SHIVAS P <shivaspanneerselvam2007@gmail.com>
2026-07-09 10:07:24 +0530

    Create 112-PathSum

---
+++ 112-PathSum/112-PathSum.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:07:23 AM
+class Solution {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val)
+            || hasPathSum(root.right, targetSum - root.val);
+    }
+}
