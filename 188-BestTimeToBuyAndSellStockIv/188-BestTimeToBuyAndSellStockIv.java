19123769d65feb8564a294b09dfd00016581183a
SHIVAS P <shivaspanneerselvam2007@gmail.com>
2026-07-09 10:06:10 +0530

    Create 188-BestTimeToBuyAndSellStockIv

---
+++ 188-BestTimeToBuyAndSellStockIv/188-BestTimeToBuyAndSellStockIv.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 10:06:09 AM
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+
+        if (n == 0) return 0;
+
+        int[][] dp = new int[k + 1][n];
+
+        for (int t = 1; t <= k; t++) {
+            int maxDiff = -prices[0];
+
+            for (int d = 1; d < n; d++) {
+                dp[t][d] = Math.max(dp[t][d - 1],
+                                    prices[d] + maxDiff);
+
+                maxDiff = Math.max(maxDiff,
+                                   dp[t - 1][d] - prices[d]);
+            }
+        }
+
+        return dp[k][n - 1];
+    }
+}
